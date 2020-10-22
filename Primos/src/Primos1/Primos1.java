bbd40cc048dfcb83b7f6a4b3342bcd6ad393fdee
kiarras <kiarras.marinero@gmail.com>
2020-10-22 23:50:04 +0200

    revision

--- Primos/src/Primos1/Primos1.java
+++ Primos/src/Primos1/Primos1.java
@@ -1,52 +1,57 @@
 package Primos1;
 
+
 public class Primos1 {
+    static final int MAXNUMBERS  =10;
     public static void main(String[] args) {
-        primosUntil50();
+        primosUntilMaxnumbers();
         first50prime();
     }
 
-    private static void primosUntil50() {
-        int[] pNumbers = new int[50];
+    private static void primosUntilMaxnumbers() {
+        int[] pNumbers = new int[MAXNUMBERS];
         int index = 0;
         for (int i = 2; i < pNumbers.length; i++) {
-            int j=2;
-            while (i%j != 0) {
-                j++;
-            }
-            if (i==j) {
+            if(isPrime(i)){
                 pNumbers[index]=i;
                 index++;
             }
         }
-        int sum=0;
-        for (int i = 0; i < pNumbers.length; i++) {
-            if(pNumbers[i]!=0) System.out.println(pNumbers[i]);
-            sum += pNumbers[i];
-        }
-            System.out.println("Suma entre los 50 primeros: " + sum);
+        printSum("Suma entre los " + MAXNUMBERS +" primeros primos: ", pNumbers);
     }
+
     private static void first50prime() {
-        int[] pNumbers = new int[50];
+        int[] pNumbers = new int[MAXNUMBERS];
         int index = 0;
         int i=2;
         do {
-            int j=2;
-            while (i%j != 0) {
-                j++;
-            }
-            if (i==j) {
+            if(isPrime(i)){
                 pNumbers[index]=i;
                 index++;
             }
             i++;
         } while (index < pNumbers.length);
 
+        printSum("Suma de los " + MAXNUMBERS +" primeros: ", pNumbers);
+    }
+    
+    private static boolean isPrime(int i) {
+        int j=2;
+        while (i%j != 0) {
+            j++;
+        }
+        if (i==j) {
+            return true;
+        }
+        return false;
+    }
+
+    private static void printSum(String title, int[] pNumbers) {
         int sum=0;
         for (int j = 0; j < pNumbers.length; j++) {
             if(pNumbers[j]!=0) System.out.println(pNumbers[j]);
             sum += pNumbers[j];
         }
-            System.out.println("Suma de los 50 primeros: " + sum);
+            System.out.println(title + sum);
     }
 }
